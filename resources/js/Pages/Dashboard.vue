3aad9eb16944006d987e858b7efdc12b3316ee7e
Skaistė <tam.skaiste@gmail.com>
2024-11-03 23:10:04 +0200

    Updated to use props tasks

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -13,133 +13,6 @@ const props = defineProps({
     },
 });
 
-const tasks = [
-    {
-        id: "1",
-        name: "PR 1 Stage 1",
-        start: "2024-10-01",
-        end: "2024-10-10",
-        progress: 50,
-        dependencies: [],
-        children: ["100", "101"],
-        display: true,
-        project_id: "1",
-        important: true,
-    },
-    {
-        id: "100",
-        name: "Sub task 1.0",
-        start: "2024-10-01",
-        end: "2024-10-15",
-        progress: 50,
-        dependencies: "",
-        display: true,
-        project_id: "1",
-        stage_id: "1",
-    },
-    {
-        id: "101",
-        name: "Sub task 1.1",
-        start: "2024-10-02",
-        end: "2024-10-06",
-        progress: 0,
-        dependencies: "",
-        display: true,
-        project_id: "1",
-        stage_id: "1",
-    },
-    {
-        id: "2",
-        name: "PR 1 Stage 2",
-        start: "2024-10-09",
-        end: "2024-10-19",
-        progress: 50,
-        dependencies: "",
-        children: ["200", "201", "202"],
-        display: true,
-        project_id: "1",
-        important: true,
-    },
-    {
-        id: "200",
-        name: "Sub task 2.0",
-        start: "2024-10-15",
-        end: "2024-10-17",
-        progress: 0,
-        dependencies: "",
-        display: true,
-        project_id: "1",
-        stage_id: "2",
-    },
-    {
-        id: "201",
-        name: "Sub task 2.1",
-        start: "2024-10-16",
-        end: "2024-10-18",
-        progress: 0,
-        dependencies: "",
-        display: true,
-        project_id: "1",
-        stage_id: "2",
-    },
-    {
-        id: "202",
-        name: "Sub task 2.2",
-        start: "2024-10-16",
-        end: "2024-10-19",
-        progress: 0,
-        dependencies: "",
-        display: true,
-        project_id: "1",
-        stage_id: "2",
-    },
-    {
-        id: "3",
-        name: "PR 2 Stage 1",
-        start: "2024-10-01",
-        end: "2024-10-10",
-        progress: 50,
-        dependencies: [],
-        children: ["300", "301", "302"],
-        display: true,
-        project_id: "2",
-        important: true,
-    },
-    {
-        id: "300",
-        name: "Sub task 1.0",
-        start: "2024-10-02",
-        end: "2024-10-10",
-        progress: 50,
-        dependencies: "",
-        display: true,
-        project_id: "2",
-        stage_id: "3",
-    },
-    {
-        id: "301",
-        name: "Sub task 1.1",
-        start: "2024-10-04",
-        end: "2024-10-06",
-        progress: 0,
-        dependencies: "",
-        display: true,
-        project_id: "2",
-        stage_id: "3",
-    },
-    {
-        id: "302",
-        name: "Milestone",
-        start: "2024-10-04",
-        end: "2024-10-04",
-        progress: 100,
-        dependencies: "",
-        display: true,
-        project_id: "2",
-        stage_id: "3",
-        custom_class: "bar-milestone",
-    },
-];
 
 const projects = ref([
     {
@@ -174,7 +47,7 @@ const calculateTaskGroupIndex = (task) => {
         task.group = increment;
         task.childCount = 0;
     } else {
-        const stage = tasks.find((t) => t.id === task.stage_id);
+        const stage = props.tasks.find((t) => t.id === task.stage_id);
         if (!stage.childCount) stage.childCount = 1;
 
         if (projects.value[projectIndex].rowCount <= stage.children.length) {
@@ -186,7 +59,7 @@ const calculateTaskGroupIndex = (task) => {
     }
 };
 
-tasks.map((task) => calculateTaskGroupIndex(task));
+props.tasks.map((task) => calculateTaskGroupIndex(task));
 
 const toggleTask = (task) => {
     if (!task.children?.length) return;
@@ -228,7 +101,7 @@ const focusTask = (task) => {
 };
 
 onMounted(() => {
-    gantt = new Gantt(".gantt-target", tasks, {
+    gantt = new Gantt(".gantt-target", props.tasks, {
         header_height: 50,
         column_width: 30,
         step: 24,
